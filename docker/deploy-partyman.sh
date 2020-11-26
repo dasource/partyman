a638f0116d6f84a7616ad2f03a2948ef0b6737c3
Juha Kovanen <juha.kovanen@gmail.com>
2020-11-26 15:26:30 +0200

    updated deploy script

--- docker/deploy-partyman.sh
+++ docker/deploy-partyman.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 set -e
 
-IFS=$'\n'
-while read -r var;do
-    export "$var"
-done < <(grep -Ev '^#|^$' .env)
+function export_envs() {
+  local envFile=${1:-.env}
+  while IFS='=' read -r key temp || [ -n "$key" ]; do
+    local isComment='^[[:space:]]*#'
+    local isBlank='^[[:space:]]*$'
+    [[ $key =~ $isComment ]] && continue
+    [[ $key =~ $isBlank ]] && continue
+    value=$(eval echo "$temp")
+    eval export "$key='$value'";
+  done < $envFile
+}
 
+export_envs
 export NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
 
-docker node update --label-add $STACK_NAME.partyman=true "$NODE_ID"
+docker node update --label-add ${STACK_NAME}.partyman=true "${NODE_ID}"
 
-sed -i "s/traefik-public/$TRAEFIK_NETWORK/" partyman.yml
-docker stack deploy -c partyman.yml $STACK_NAME
+sed -i "s/traefik-public/${TRAEFIK_NETWORK}/" partyman.yml
+docker stack deploy -c partyman.yml ${STACK_NAME}
 
-echo "You should now be able to access: https://partyman.$UI_DOMAIN"
+echo "You should now be able to access: https://partyman.${UI_DOMAIN}"
+
